9527151ae55125429cf6fec9f1bf78aefe309f76
Colin <colin.baus@kit.edu>
2013-11-11 16:08:29 +0100

    updated PAS analysis status before final MPI approval. hundreds of changes. mostly reweighting by lev's eta plot

---
+++ makePlots_perf_ring_corr_factors.C
@@ -0,0 +1,172 @@
+///HF plots E over eta
+
+#include "TFitResultPtr.h"
+#include "TFitResult.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TMarker.h"
+#include "TVectorD.h"
+#include "TROOT.h"
+#include "TMath.h"
+
+#ifndef __CINT__
+#include "style.h"
+#endif
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+void Show(TH1D* a,TH1D* b,TH1D* c, TH1D* epossl, TH1D* d, string type);
+int BinToIeta(int bin);
+int IetaToRing(int ieta);
+int RingToIeta(int ring);
+
+TVectorD hf_calibration(24);
+
+
+void makePlots_perf_ring_corr_factors()
+{
+  gROOT->ProcessLine(" .L style.cc+");
+  style();
+
+  for(int ring=0; ring<24; ring++)
+    {
+      hf_calibration[ring]=1;
+    }
+  cout << "\\hline Ieta & calibration factor\\\\\\hline\\hline" << endl; 
+
+  TFile* file = TFile::Open("histos2.root");
+  TFile* file2 = TFile::Open("histos2.root");
+  TH1D* e= (TH1D*)file->Get("data210885/data210885_h_perf_hf_totE_eta_lev_m");
+  TH1D* f= (TH1D*)file->Get("Hijing/Hijing_h_perf_hf_totE_eta_lev_m");
+  TH1D* g= (TH1D*)file->Get("Epos/Epos_h_perf_hf_totE_eta_lev_m");
+  TH1D* epossl= (TH1D*)file->Get("Epos_SL/Epos_SL_h_perf_hf_totE_eta_lev_m");
+  TH1D* h= (TH1D*)file->Get("QGSJetII/QGSJetII_h_perf_hf_totE_eta_lev_m");
+
+  Show(e,f,g,epossl,h,"lev_minus");
+
+  TFile* file = TFile::Open("histos2.root");
+  TFile* file2 = TFile::Open("histos2.root");
+  TH1D* e= (TH1D*)file->Get("data210885/data210885_h_perf_hf_totE_eta_lev_p");
+  TH1D* f= (TH1D*)file->Get("Hijing/Hijing_h_perf_hf_totE_eta_lev_p");
+  TH1D* g= (TH1D*)file->Get("Epos/Epos_h_perf_hf_totE_eta_lev_p");
+  TH1D* epossl= (TH1D*)file->Get("Epos_SL/Epos_SL_h_perf_hf_totE_eta_lev_p");
+  TH1D* h= (TH1D*)file->Get("QGSJetII/QGSJetII_h_perf_hf_totE_eta_lev_p");
+
+  Show(e,f,g,epossl,h,"lev_plus");
+  
+  hf_calibration.Print();
+  TFile calibfile("plots/hf_calibration_data.root","recreate");
+  hf_calibration.Write("hf_calibration");
+  calibfile.Close();
+}
+
+void Show(TH1D* a, TH1D* b, TH1D* c, TH1D* epossl, TH1D* d, string type)
+{
+  //a->Scale(a->GetBinContent(2)/a->GetBinContent(2);
+  int bin = 2;
+  if (type=="lev_minus")
+    bin = 12;
+  b->Scale(a->GetBinContent(bin)/b->GetBinContent(bin));
+  c->Scale(a->GetBinContent(bin)/c->GetBinContent(bin));
+  epossl->Scale(a->GetBinContent(bin)/epossl->GetBinContent(bin));
+  d->Scale(a->GetBinContent(bin)/d->GetBinContent(bin));
+
+  a->SetMarkerSize(1.2);
+  a->SetLineWidth(2.5);
+  b->SetLineWidth(2.5);
+  c->SetLineWidth(2.5);
+  epossl->SetLineWidth(2.5);
+  d->SetLineWidth(2.5);
+
+
+  a->SetMarkerColor(kBlack);
+  b->SetMarkerColor(kRed);
+  c->SetMarkerColor(kBlue);
+  epossl->SetMarkerColor(kCyan);
+  d->SetMarkerColor(kGreen+2);
+
+  a->SetLineColor(kBlack);
+  b->SetLineColor(kRed);
+  c->SetLineColor(kBlue);
+  epossl->SetLineColor(kCyan);
+  d->SetLineColor(kGreen+2);
+
+  a->SetTitle("zero bias");
+  b->SetTitle("HIJING");
+  c->SetTitle("EPOS");
+  epossl->SetTitle("EPOS (SL)");
+  d->SetTitle("QGSJetII");
+
+  double maximumy = 1.2 * TMath::Max(TMath::Max(a->GetMaximum(),b->GetMaximum()),TMath::Max(c->GetMaximum(),d->GetMaximum()));
+  a->GetYaxis()->SetRangeUser(0,60);
+  a->GetXaxis()->SetTitle("#eta");
+  a->GetYaxis()->SetTitle("E [GeV]");
+
+  TCanvas* c1 = new TCanvas;
+  a->Draw("HIST P");
+  b->Draw("HIST L SAME");
+  c->Draw("HIST L SAME");
+  epossl->Draw("HIST L SAME");
+  d->Draw("HIST L SAME");
+  TLegend* leg = new TLegend(0.3,0.7,0.7,0.9);
+  leg->AddEntry(a,"","P");
+  leg->AddEntry(b,"","L");
+  leg->AddEntry(c,"","L");
+  leg->AddEntry(epossl,"","L");
+  leg->AddEntry(d,"","L");
+  SetLegAtt(leg);
+  leg->SetFillColor(kWhite);
+  leg->Draw();
+
+
+  for(int bin=2;bin<=12;bin++)
+    {
+      double avg = b->GetBinContent(bin) + c->GetBinContent(bin) + d->GetBinContent(bin);
+      avg/=3.;
+      
+      int ieta = BinToIeta(type=="lev_minus"?(-bin):bin);
+      
+      double calib = avg/a->GetBinContent(bin);
+      cout << ieta << " & " << setprecision(3) <<  calib << "\\\\" << endl;
+      hf_calibration[IetaToRing(ieta)] = calib;
+    }
+  cout << "\\hline" << endl;
+           
+}
+int BinToIeta(int bin)
+///converts internal bin numbebin to original ieta
+{
+  if(bin < 0)
+    return -40 - 2 - bin;
+  else
+    return bin - 12 + 40;
+}
+
+int IetaToRing(int ieta)
+///converts ieta to numbers from 0 to 23
+{
+  //rings range from [-41,29] and [29,41]
+  //29 is skipped in trees
+  //41 is skipped usually because of HCAL prescription
+  if(ieta < 0)
+    return ieta + 41;
+  else
+    return ieta + 12 - 30;
+}
+
+int RingToIeta(int ring)
+///converts rings from 0 to 23 to ieta
+{
+  //rings range from [-41,29] and [29,41]
+  //29 is skipped in trees
+  //41 is skipped usually because of HCAL prescription
+  if(ring < 12)
+    return ring - 41;
+  else
+    return ring - 12 + 30;
+}
---
+++ makePlots_perf_triggered_ring.C
@@ -0,0 +1,158 @@
+///Plot the stacked hit distribution of (noise,em + data/mc) for HF
+
+
+void Show(TH1D* a,TH1D* a2,TH1D* b,TH1D* c,TH1D* d,TH1D* e,TH1D* f,string type);
+void ShowStack(TH1D* a,TH1D* a2,TH1D* b,TH1D* c,TH1D* d,TH1D* e,TH1D* f,string type);
+TH1D* merge(TH1D* bg1, TH1D* bg2, TH1D* signal);
+
+void makePlots_perf_triggered_ring()
+{
+  gROOT->ProcessLine(" .L style.cc+");
+  style();
+
+  {
+    TFile* file2 = TFile::Open("histos3.root");
+    TFile* file = TFile::Open("histos3.root");
+  TH1D* a=(TH1D*)file2->Get("data210885/data210885_h_hf_triggered_ring_single");
+  TH1D* a2=(TH1D*)file2->Get("data210885/data210885_h_hf_triggered_ring_noise_single");
+  TH1D* b=(TH1D*)file->Get("Hijing/Hijing_h_hf_triggered_ring_single");
+  TH1D* c=(TH1D*)file->Get("Epos/Epos_h_hf_triggered_ring_single");
+  //TH1D* c=(TH1D*)file->Get("Epos_SL/Epos_SL_h_hf_triggered_ring_single");
+  TH1D* d=(TH1D*)file->Get("QGSJetII/QGSJetII_h_hf_triggered_ring_single");
+  TH1D* e=(TH1D*)file->Get("Starlight_DPMJet/Starlight_DPMJet_h_hf_triggered_ring_single");
+  TH1D* f=(TH1D*)file->Get("Starlight_Pythia/Starlight_Pythia_h_hf_triggered_ring_single");
+  ShowStack(a,a2,b,c,d,e,f,"single");
+  }
+  {
+  TFile* file2 = TFile::Open("histos3.root");
+  TFile* file = TFile::Open("histos3.root");
+  TH1D* a=(TH1D*)file2->Get("data210885/data210885_h_hf_triggered_ring_double");
+  TH1D* a2=(TH1D*)file2->Get("data210885/data210885_h_hf_triggered_ring_noise_double");
+  TH1D* b=(TH1D*)file->Get("Hijing/Hijing_h_hf_triggered_ring_double");
+  TH1D* c=(TH1D*)file->Get("Epos/Epos_h_hf_triggered_ring_double");
+  //TH1D* c=(TH1D*)file->Get("Epos_SL/Epos_SL_h_hf_triggered_ring_double");
+  TH1D* d=(TH1D*)file->Get("QGSJetII/QGSJetII_h_hf_triggered_ring_double");
+  TH1D* e=(TH1D*)file->Get("Starlight_DPMJet/Starlight_DPMJet_h_hf_triggered_ring_double");
+  TH1D* f=(TH1D*)file->Get("Starlight_Pythia/Starlight_Pythia_h_hf_triggered_ring_double");
+  ShowStack(a,a2,b,c,d,e,f,"double");
+  }
+}
+
+void ShowStack(TH1D* data,TH1D* noise,TH1D* b,TH1D* c,TH1D* d,TH1D* sl1,TH1D* sl2, string type)
+  {
+    cout << data << " " << noise << " " << b << " " << c << " " << d << " " << sl1 << " " << sl2 << endl;
+  double eposscale=double(c->Integral())/double(sl1->Integral());
+  double eposscale2=double(c->Integral())/double(sl2->Integral());
+  noise->Scale(1./double(data->Integral()) * 48.216 / (9.9845*(1.-(84.+296.)/3568.))); //HLT PAAccept rate is 48.2216HZ compared to 9.9845Hz of noise //(84+296)/3568 get skipped because pf BPTX_quiet
+  data->Scale(1./double(data->Integral()));
+  b->Scale(data->Integral()/b->Integral());
+  double eposnorm=data->Integral()/c->Integral();
+  c->Scale(eposnorm);
+  d->Scale(data->Integral()/d->Integral());
+  sl1->Scale(eposscale*195./2130.*eposnorm);
+  sl2->Scale(eposscale2*122./2130.*eposnorm);
+  sl1->SetBit(TH1::kIsAverage);
+  sl2->SetBit(TH1::kIsAverage);
+  if(sl1->Add(sl2) == kFALSE) {new TCanvas; sl1->Draw(); new TCanvas; sl2->Draw(); cerr << "add failed" << endl; return;}
+  TH1D* sl = sl1;
+
+  //data->Add(noise,-1);
+  data->SetMarkerSize(1);
+
+  // data->SetLineWidth(2);
+  // noise->SetLineWidth(1.5);
+  // b->SetLineWidth(2);
+  // c->SetLineWidth(2);
+  // d->SetLineWidth(2);
+  // sl->SetLineWidth(2);
+
+
+  data->SetMarkerColor(kBlack);
+  noise->SetMarkerColor(kGreen-10);
+  b->SetMarkerColor(kRed);
+  c->SetMarkerColor(kBlue);
+  d->SetMarkerColor(kGreen+2);
+  sl->SetMarkerColor(kOrange-9);
+
+  noise->SetMarkerStyle(34);
+  b->SetMarkerStyle(4);
+  c->SetMarkerStyle(25);
+  d->SetMarkerStyle(28);
+  sl->SetMarkerStyle(22);
+
+  data->SetLineColor(data->GetMarkerColor());
+  noise->SetLineColor(noise->GetMarkerColor());
+  b->SetLineColor(b->GetMarkerColor());
+  c->SetLineColor(c->GetMarkerColor());
+  d->SetLineColor(d->GetMarkerColor());
+  sl->SetLineColor(sl->GetMarkerColor());
+
+  data->SetFillColor(data->GetMarkerColor());
+  noise->SetFillColor(noise->GetMarkerColor());
+  b->SetFillColor(b->GetMarkerColor());
+  c->SetFillColor(c->GetMarkerColor());
+  d->SetFillColor(d->GetMarkerColor());
+  sl->SetFillColor(sl->GetMarkerColor());
+
+
+  data->SetTitle("Data");
+  noise->SetTitle("Noise");
+  b->SetTitle("HIJING 1.383");
+  c->SetTitle("EPOS-LHC");
+  d->SetTitle("QGSJetII-04");
+  sl->SetTitle("#gamma-p (STARLIGHT+DPMJET/PYTHIA)");
+
+  //data->GetXaxis()->SetLimits(1,);
+  data->GetYaxis()->SetRangeUser(0,0.13);
+  data->GetXaxis()->SetTitle("HF Ring (ieta)");
+  data->GetYaxis()->SetTitle("events (normalised)");
+  data->GetXaxis()->SetTitleOffset(data->GetXaxis()->GetTitleOffset()*1.1);
+
+
+  THStack* h_s_bg = new THStack("h_s_gb","events");
+  h_s_bg->Add(noise,"HIST F");
+  h_s_bg->Add(sl,"HIST F");
+
+  if(0)
+    {
+      b = merge(noise,sl,b);
+      c = merge(noise,sl,c);
+      d = merge(noise,sl,d);
+    }
+
+  TCanvas* c1 = new TCanvas;
+  data->Draw("P");
+  h_s_bg->Draw("SAME");
+  b->Draw("SAME");
+  c->Draw("SAME");
+  d->Draw("SAME");
+  data->Draw("SAME P");
+  data->Draw("SAME AXIS");
+
+  TLegend* leg = new TLegend(0.23,0.72,0.43,0.93);
+  SetLegAtt(leg);
+  leg->AddEntry(data,"","p");
+  leg->AddEntry(b,"","p");
+  leg->AddEntry(c,"","p");
+  leg->AddEntry(d,"","p");
+  leg->AddEntry(sl,"","f");
+  leg->AddEntry(noise,"","f");
+  leg->Draw();
+  CMSText(1,0,1,type=="single"?"single-arm selection":"double-arm selection");
+
+  c1->SaveAs((string("plots/hf_") + type + string("_triggered_rings")+string(".pdf")).c_str());
+
+}
+
+TH1D* merge(TH1D* bg1, TH1D* bg2, TH1D* signal)
+{
+  TList *list = new TList;
+  list->Add(bg1);
+  list->Add(bg2);
+  list->Add(signal);
+  ostringstream newname; newname << signal->GetName() << "_merged";
+  TH1D* newsignal = (TH1D*)signal->Clone(newname.str().c_str());
+  newsignal->Reset();
+  newsignal->Merge(list);
+  return newsignal;
+}
